516dd2f5ac6b0f60c3afd4e84650eb07945a1356
Nakamura Masashi <masashi@nds-tyo.co.jp>
2025-04-28 10:19:20 +0000

    mock生成に環境変数使用

--- youtubeapi_ddd/domain/service/yotube_resolver/yotube_resolver.go
+++ youtubeapi_ddd/domain/service/yotube_resolver/yotube_resolver.go
@@ -3,7 +3,7 @@ package yotube_resolver
 
 import "github.com/nds-masashi/sr/domain/model/video"
 
-//go:generate mockgen -source=yotube_resolver.go -destination=yotube_resolver_mock.go -package=yotube_resolver
+//go:generate mockgen -source=$GOFILE -package=$GOPACKAGE -destination=./$GOPACKAGE.mock.go
 
 type Service interface {
 	GetVideoUrl(title string, limit int) ([]video.Model, error)
